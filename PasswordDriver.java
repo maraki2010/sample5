ad5668dd7b4f9694af66a5e04b759dabc3844f16
Yihenew <77293693+maraki2010@users.noreply.github.com>
2021-01-11 14:18:22 -0500

    Create PasswordDriver.java
    Just add java sample

---
+++ PasswordDriver.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class PasswordDriver
+{
+   public static void main(String[] args)
+   {
+      String password;
+      
+      // input a password
+      password = JOptionPane.showInputDialog("Choose a Password: \n "+
+                                             "Passwords must be at least 6 characters long \n" +
+                                             "Have at least 1 UPPERCASE letter \n" +
+                                             "Have at least 1 lowerCASE letter \n" +
+                                             "And have at least 1 numerical digit \n\n" +
+                                              "Enter a Password: ");
+      // determine whether or not the password is valid
+      if(PasswordVerifier.validPassword(password))
+         JOptionPane.showMessageDialog(null, password + " is valid");
+      else
+         JOptionPane.showMessageDialog(null, password + " is invalid");
+   }
+}
+//working with github
